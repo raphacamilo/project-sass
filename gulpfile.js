b072fafdfbcbfb0a63172047528b78299ff8ff7f
raphael <raphael.camilo@apto.vc>
2021-07-12 18:30:24 -0300

    added browsersync in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,41 @@
 const gulp = require("gulp");
 const gulpSass = require("gulp-sass");
 const dartSass = require("sass");
+const browserSync = require("browser-sync").create();
 
 const sass = gulpSass(dartSass);
 
 gulp.task("sass", sassCompiler);
-gulp.task("watch", watch);
+
+gulp.task("browserSync", function (done) {
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "./src",
+    },
+  });
+  done();
+});
+
+gulp.task(
+  "watch",
+  gulp.series("browserSync", "sass", function () {
+    gulp.watch("src/scss/**/*.scss", sassCompiler);
+    gulp.watch("src/*.html", { events: "all" }, function (cb) {
+      browserSync.reload();
+      cb();
+    });
+  })
+);
 
 function sassCompiler() {
   return gulp
-    .src("scss/**/*.scss")
+    .src("src/scss/**/*.scss")
     .pipe(sass())
-    .pipe(gulp.dest("css/style.css"));
-}
-
-function watch() {
-  gulp.watch("scss/**/*.scss", sassCompiler);
+    .pipe(gulp.dest("src/css/"))
+    .pipe(
+      browserSync.reload({
+        stream: true,
+      })
+    );
 }
