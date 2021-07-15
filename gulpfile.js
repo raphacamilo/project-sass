4554bf0d27c325e6a430a5f9dcacf32a3f351495
raphael <raphael.camilo@apto.vc>
2021-07-14 21:30:50 -0300

    added cssMinify automation

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,14 @@ const gulpSass = require("gulp-sass");
 const dartSass = require("sass");
 const browserSync = require("browser-sync").create();
 
+const postcss = require("gulp-postcss");
+const cssnano = require("cssnano");
+const autoprefixer = require("autoprefixer");
+const rename = require("gulp-rename");
+
 const sass = gulpSass(dartSass);
 
-gulp.task("sass", sassCompiler);
+gulp.task("sass", cssMinify);
 
 gulp.task("browserSync", function browserInit(done) {
   browserSync.init({
@@ -23,7 +28,7 @@ gulp.task("browserSync", function browserInit(done) {
 gulp.task(
   "watch",
   gulp.series("browserSync", "sass", function Watch() {
-    gulp.watch("src/assets/scss/**/*.scss", sassCompiler);
+    gulp.watch("src/assets/scss/**/*.scss", gulp.series("sass"));
     gulp.watch("src/*.html", { events: "all" }, siteReload);
     gulp.watch("src/assets/css/**/*.css", { events: "all" }, siteReload);
     gulp.watch("src/assets/js/**/*.js", { events: "all" }, siteReload);
@@ -38,7 +43,26 @@ function siteReload(cb) {
 function sassCompiler() {
   return gulp
     .src("src/assets/scss/**/*.scss")
-    .pipe(sass())
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("src/assets/css"))
+    .pipe(
+      browserSync.reload({
+        stream: true,
+      })
+    );
+}
+
+function cssMinify() {
+  return gulp
+    .src("src/assets/scss/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("src/assets/css"))
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(
+      rename({
+        extname: ".min.css",
+      })
+    )
     .pipe(gulp.dest("src/assets/css"))
     .pipe(
       browserSync.reload({
